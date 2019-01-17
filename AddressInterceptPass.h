1d13ceb127a155457fbb877d9d6a6f8bb664b879
Evgeny Chormonov <en.chormonov@gmail.com>
2019-01-17 23:14:23 +0300

    FIX: clang can't definition __adin_store_ and __adin_load_

--- AddressInterceptPass.h
+++ AddressInterceptPass.h
@@ -8,11 +8,8 @@ typedef  uint8_t * llvm_pass_addr;
 
 typedef uintptr_t llvm_ocd_addr;
 
+extern "C" void __adin_store_(llvm_pass_addr pointer, llvm_pass_arg value, llvm_pass_arg TypeSizeArg, llvm_pass_arg AlignmentArg);
 
-
-
-void __adin_store_(llvm_pass_addr pointer, llvm_pass_arg value, llvm_pass_arg TypeSizeArg, llvm_pass_arg AlignmentArg);
-
-llvm_pass_arg __adin_load_(llvm_pass_addr pointer, llvm_pass_arg TypeSizeArg, llvm_pass_arg AlignmentArg);
+extern "C" llvm_pass_arg __adin_load_(llvm_pass_addr pointer, llvm_pass_arg TypeSizeArg, llvm_pass_arg AlignmentArg);
 
 #endif // LLVM_H
