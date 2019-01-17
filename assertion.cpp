8728a35184c11358a77afdc835c9f68d9e12c23c
Evgeny Chormonov <en.chormonov@gmail.com>
2019-01-18 02:44:30 +0300

    FEAT: add assertions

--- assertion.cpp
+++ assertion.cpp
@@ -1,6 +1,28 @@
 #include "assertion.h"
+#include <assert.h>
 
-assertion::assertion()
-{
+namespace adin {
 
+static error_function_t callback = NULL;
+
+void setErrorFunction(error_function_t fun){
+    callback = fun;
 }
+
+bool existErrorCallback(){
+    return callback != NULL;
+}
+
+bool __callback(const char *__assertion, const char *__file,
+                unsigned int __line, const char *__function){
+    return (*callback)(__assertion, __file, __line, __function);
+}
+
+bool __assertionF (const char *__assertion, const char *__file,
+                  unsigned int __line, const char *__function){
+
+    __assert_fail (__assertion, __file, __line, __function);
+    return true;
+}
+
+} //namespace
--- assertion.h
+++ assertion.h
@@ -1,11 +1,32 @@
 #ifndef ASSERTION_H
 #define ASSERTION_H
+#include <assert.h>
+#include "logger.h"
+
+namespace adin {
 
 
-class assertion
-{
-public:
-assertion();
-};
+void __ASSERT_DEBUG(const char *);
+
+bool existErrorCallback();
+
+bool __callback(const char *__assertion, const char *__file,
+                unsigned int __line, const char *__function);
+
+bool __assertionF (const char *__assertion, const char *__file,
+                  unsigned int __line, const char *__function);
+
+
+
+//ADIN_PRINTF(_ALL_LOG,"%s",#expr)
+} //namespace
+
+# define asser_1line(expr)							\
+  ((expr)								\
+   ? 	ADIN_PRINTF(_ALL_LOG,"%s\n", #expr)			\
+   :  existErrorCallback()   \
+    ?  __callback(#expr, __FILE__, __LINE__, __ASSERT_FUNCTION) \
+    : __assertionF(#expr, __FILE__, __LINE__, __ASSERT_FUNCTION))    \
+
 
 #endif // ASSERTION_H
