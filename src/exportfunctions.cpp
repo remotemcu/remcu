97165066abc3d3e3a3e69f2900ec3f53d9a947e1
Evgeny Chormonov <en.chormonov@gmail.com>
2019-02-13 17:33:15 +0300

    FEAT: add STM32F40_41xxx config

--- src/exportfunctions.cpp
+++ src/exportfunctions.cpp
@@ -56,6 +56,14 @@ bool setMCU(const std::string target){
     if(target.compare("STM32F10X_MD") == 0){
         addInterceptAddress2Interval(0x20000000, 0x20000000 + 20*1024);
         targetMCU.assign(target);
+    } else  if(target.compare("STM32F40_41xxx") == 0){
+        addInterceptAddress2Interval(0x20000000,  0x20000000 + (112)*1024); //SRAM
+        addInterceptAddress2Interval(0x40000000,  0x40008000); //APB1
+        addInterceptAddress2Interval(0x40010000,  0x40016C00); //APB2
+        addInterceptAddress2Interval(0x40020000,  0x40080000); //AHB1
+        addInterceptAddress2Interval(0x50000000,  0x50060C00); //AHB2
+        addInterceptAddress2Interval(0xA0000000,  0xA0001000); //AHB3
+        targetMCU.assign(target);
     } else {
         return false;
     }
