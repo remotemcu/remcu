1330ecb1943eb807613c6324abe06fb0653dc833
Evgeny Chormonov <en.chormonov@gmail.com>
2019-02-26 05:54:17 +0300

    IMPR: cmake generate sign file license

--- cmake/license.cmake
+++ cmake/license.cmake
@@ -8,10 +8,6 @@ if(NOT LICENSE_TYPE)
 endif()
 
 
-# Strip lead and trailing whitepasce
-#string(STRIP ${LICENSE_TYPE} LICENSE_TYPE)
-
-
 if(LICENSE_TYPE STREQUAL "EDU")
 	message( "******* EDUCATION license ********")
 elseif(LICENSE_TYPE STREQUAL "COM")
@@ -20,4 +16,24 @@ else()
 	message(FATAL_ERROR "******* unknown license '${LICENSE_TYPE}' ********")
 endif()
 
-add_definitions(-DLICENSE_TYPE=\"${LICENSE_TYPE}\" )
+if(NOT LICENSE_FILE_PATH)
+	if(NOT ENV{LICENSE_FILE_PATH})
+		set(LICENSE_FILE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/license_text/${LICENSE_TYPE}_LICENSE.txt)
+	else()
+		set(LICENSE_FILE_PATH ENV{LICENSE_FILE_PATH})
+	endif()
+endif()
+
+
+add_definitions(-DLICENSE_TYPE=\"${LICENSE_TYPE}\")
+
+set(LICENSE_SIGN_SCRIPT license.py)
+set(LICENSE_SIGN_FILE REMCU_LICENSE.txt)
+
+add_custom_target (
+  ${LICENSE_SIGN_FILE}
+  COMMAND python ${CMAKE_CURRENT_SOURCE_DIR}/${LICENSE_SIGN_SCRIPT}
+  -int ${LICENSE_FILE_PATH}
+  -out ${CMAKE_CURRENT_BINARY_DIR}/${LICENSE_SIGN_FILE}
+  -t ${LICENSE_TYPE}
+  )
