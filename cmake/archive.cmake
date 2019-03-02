6fdd61ac46e210de2cef2c5f71bf57a1f437240e
Evgeny Chormonov <en.chormonov@gmail.com>
2019-03-03 00:57:23 +0300

    IMPR: archive zip for windows

--- cmake/archive.cmake
+++ cmake/archive.cmake
@@ -1,3 +1,6 @@
 
-set(ARCHIVE_COMMAND tar -cf ${FULL_VERSION_NAME}.tar)
-
+if(WIN32)
+	set(ARCHIVE_COMMAND zip -r ${FULL_VERSION_NAME}.zip)
+else()
+	set(ARCHIVE_COMMAND tar -cf ${FULL_VERSION_NAME}.tar)
+endif()
