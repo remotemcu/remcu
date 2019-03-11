b80d0a515fc28e64f05b417e550beb88ad1f42f5
Evgeny Chormonov <en.chormonov@gmail.com>
2019-03-11 20:47:20 +0300

    IMPR: copy target files in cmake of target

--- cmake/mcu.cmake
+++ cmake/mcu.cmake
@@ -7,10 +7,6 @@ if(NOT DEFINED MCU_TYPE)
 	message(FATAL_ERROR "******* $MCU_TYPE is not defined! ********")
 endif()
 
-if(NOT DEFINED COPY_SOURCE_FROM_LIB)
-	message(FATAL_ERROR "******* $COPY_SOURCE_FROM_LIB is not defined! ********")
-endif()
-
 if(NOT DEFINED CONF_SOURCE)
 	message(FATAL_ERROR "******* $CONF_SOURCE is not defined! ********")
 endif()
