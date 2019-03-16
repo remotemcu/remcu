cfcc5a10d40cacfb9fd0d483868fea8f3f3e190e
Evgeny Chormonov <en.chormonov@gmail.com>
2019-03-16 23:38:22 +0300

    FIX: print expr msg in release version

--- include/assertion.h
+++ include/assertion.h
@@ -7,41 +7,40 @@
 
 namespace remcu {
 
-#if 0
-void __ASSERT__DEBUG(const char *);
-
-bool existErrorCallback();
-
-bool __callback(const char *__assertion, const char *__file,
-                const unsigned int __line, const char *__function);
-
-bool __assertionF (const char *__assertion, const char *__file,
-                  const unsigned int __line, const char *__function);
-#endif
 
 #ifdef NDEBUG
+
     #define assert_1message(expr, msg)                                      \
       if(expr == false){                                                         \
             ADIN_LOG(__ERROR) << _S_(msg);                                        \
             return false;                                                   \
         }
+
+    #define assert_printf(expr, msg,...)                                      \
+      if(expr == false){                                                      \
+            ADIN_PRINTF(__ERROR, msg, __VA_ARGS__);                      \
+            return false;                                                   \
+        }                                                                   \
+
 #else
+
     #define assert_1message(expr, msg)                                      \
       if(expr){                                                             \
-            ADIN_PRINTF(__ALL_LOG,"%s\n", #expr);                            \
+            ADIN_PRINTF(__ALL_LOG,"utv: %s\n", #expr);                            \
         } else {                                                            \
             ADIN_LOG(__ERROR) << _S_(msg);                                        \
             return false;                                                   \
         }
-#endif
 
-# define assert_printf(expr, msg,...)                                      \
-  if(expr){                                                             \
-        ADIN_PRINTF(__ALL_LOG,"%s\n", #expr);                            \
-    } else {                                                            \
-        ADIN_PRINTF(__ERROR, msg, __VA_ARGS__);                          \
-        return false;                                                   \
-    }                                                                   \
+    #define assert_printf(expr, msg,...)                                      \
+      if(expr){                                                             \
+            ADIN_PRINTF(__ALL_LOG,"utv: %s\n", #expr);                            \
+        } else {                                                            \
+            ADIN_PRINTF(__ERROR, msg, __VA_ARGS__);                          \
+            return false;                                                   \
+        }                                                                   \
+
+#endif
 
 } //namespace
 
