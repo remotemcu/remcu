5cffc579975f412cfb56d661c3af7d0d6683e0f5
Evgeny Chormonov <en.chormonov@gmail.com>
2019-04-20 20:32:31 +0300

    IMPR: check include REMCU_EXPORTS_SYMBOL_ENTER

--- mcu_utils/include_utils/remcu_exports_symbol_enter.h
+++ mcu_utils/include_utils/remcu_exports_symbol_enter.h
@@ -1,3 +1,8 @@
-#if (defined(_WIN32) || defined(WIN32) || defined(WIN64) || defined(_WIN64))
-	#pragma clang attribute push (__declspec(dllexport), apply_to = function)
+#ifdef REMCU_EXPORTS_SYMBOL_ENTER
+    #error "REMCU_EXPORTS_SYMBOL_ENTER repeat"
+#else
+#define REMCU_EXPORTS_SYMBOL_ENTER
+    #if (defined(_WIN32) || defined(WIN32) || defined(WIN64) || defined(_WIN64))
+            #pragma clang attribute push (__declspec(dllexport), apply_to = function)
+    #endif
 #endif
--- mcu_utils/include_utils/remcu_exports_symbol_exit.h
+++ mcu_utils/include_utils/remcu_exports_symbol_exit.h
@@ -1,3 +1,8 @@
-#if (defined(_WIN32) || defined(WIN32) || defined(WIN64) || defined(_WIN64))
-	#pragma clang attribute pop
+#ifdef REMCU_EXPORTS_SYMBOL_ENTER
+    #if (defined(_WIN32) || defined(WIN32) || defined(WIN64) || defined(_WIN64))
+        #pragma clang attribute pop
+    #endif
+    #undef REMCU_EXPORTS_SYMBOL_ENTER
+#else
+    #error "REMCU_EXPORTS_SYMBOL_ENTER absent"
 #endif
