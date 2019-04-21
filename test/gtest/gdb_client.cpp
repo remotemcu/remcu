4776aca678a5bab13344c881ce0250dd30a89f19
Evgeny Chormonov <en.chormonov@gmail.com>
2019-04-21 14:26:56 +0300

    FEAT: add gtest

---
+++ test/gtest/gdb_client.cpp
@@ -0,0 +1,11 @@
+#include "gtest/gtest.h"
+
+// Tests factorial of 0.
+TEST(succesful, test) {
+    EXPECT_EQ(0, 0);
+}
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
