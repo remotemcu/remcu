ed91f517a8359d9b44e05340b9f648316c3d40fd
Tom Brass <en.chormonov@gmail.com>
2020-04-01 15:29:12 +0300

    FEATUR: add ccache for linux

--- cmake/common_linux_flags.cmake
+++ cmake/common_linux_flags.cmake
@@ -14,3 +14,11 @@ set(CMAKE_CXX_FLAGS_RELEASE ${CMAKE_CXX_FLAGS_RELEASE} "-O2 -DNDEBUG -pthread -f
 
 set(CMAKE_CXX_FLAGS  ${CMAKE_CXX_FLAGS} "-fno-rtti -fno-exceptions -fPIC " 
 	CACHE STRING "Linux x64 CMAKE_CXX_FLAGS: ${CMAKE_CXX_FLAGS}")
+
+
+# Configure CCache if available
+find_program(CCACHE_FOUND ccache)
+if(CCACHE_FOUND)
+        set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ccache)
+        set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ccache)
+endif(CCACHE_FOUND)
