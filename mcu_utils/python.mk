679c8fc6dbeabb52cb59b440b428bd636eb14be4
Tom Brass <en.chormonov@gmail.com>
2020-05-10 01:52:52 +0300

    IMRP: python parsingin build time. once

---
+++ mcu_utils/python.mk
@@ -0,0 +1,10 @@
+
+C_FILES  += $(addprefix -a ,$(INC_PATH))
+PYTHON_HEADER_SCRIPT = ~/MOCD/AddressInterceptor/mcu-lib/parser/header2.py
+PY_VERSION = --version=$(MCU_MAJOR_VERSION_LIB)-`git rev-parse --short HEAD`
+NAME_PY_FILE = ${DEVICE}_$(MCU_LIB_NAME).py
+
+python: $(NAME_PY_FILE)
+
+$(NAME_PY_FILE):
+	python $(PYTHON_HEADER_SCRIPT) -c "$(LOCAL_CFLAGS)" $(C_FILES) $(PY_VERSION) -o $(NAME_PY_FILE) -d 1
