976aecfac3345a565b9fda410fda7c54132d926c
Evgeny Chormonov <en.chormonov@gmail.com>
2023-06-19 18:08:24 +0300

    REFRACTORING: del print message

--- platform/vars.cmake
+++ platform/vars.cmake
@@ -5,8 +5,4 @@ else()
   message(FATAL_ERROR "Not set LLVM_ADIN_PATH env var")
 endif()
 
-include(CMakePrintHelpers)
-cmake_print_variables(CMAKE_CURRENT_LIST_DIR)
-
-
 set(REMCU_VM_PATH ${CMAKE_CURRENT_LIST_DIR}/../)
