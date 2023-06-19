fc9610ee2852a24c9e530c147ad0378214347f71
Evgeny Chormonov <en.chormonov@gmail.com>
2023-06-19 21:44:55 +0300

    FEATURE: add macos darwin x64 cmake toolchain file

---
+++ platform/macos_darwin_x64.cmake
@@ -0,0 +1,12 @@
+
+include(${CMAKE_CURRENT_LIST_DIR}/vars.cmake)
+
+set(MCU_INCLUDE_MK_FILE ${REMCU_VM_PATH}/mcu_utils/linux.mk)
+# set(STRIP_COMMAND strip)
+
+include(${REMCU_VM_PATH}/cmake/common_linux_flags.cmake)
+#set(STRIP_ARGUMENT -NSXxT)
+
+set(TARGET_REMCU_OS_NAME MacOS_Darwin_x64)
+
+set(LICENSE_TYPE APACHE_2.0)
