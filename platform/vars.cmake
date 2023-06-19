c6910a92ff28d4b66fbe94d37b8925642039b564
Evgeny Chormonov <en.chormonov@gmail.com>
2023-06-19 18:23:17 +0300

    IMPR: check opt bin in cmake toolchain file

--- platform/vars.cmake
+++ platform/vars.cmake
@@ -5,4 +5,19 @@ else()
   message(FATAL_ERROR "Not set LLVM_ADIN_PATH env var")
 endif()
 
+find_program(RESULT_FIND_OPT
+  NAME opt
+  PATHS ${LLVM_ADIN_PATH}
+  NO_DEFAULT_PATH
+  REQUIRED
+  )
+
+include(CMakePrintHelpers)
+cmake_print_variables(RESULT_FIND_OPT)
+
+if(NOT RESULT_FIND_OPT)
+    message(FATAL_ERROR "opt binary not found at LLVM_ADIN_PATH path: ${LLVM_ADIN_PATH}")
+endif()
+
+
 set(REMCU_VM_PATH ${CMAKE_CURRENT_LIST_DIR}/../)
