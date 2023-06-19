deb3432e22bfcd157eebfb586b329ec40e40540b
Evgeny Chormonov <en.chormonov@gmail.com>
2023-06-19 18:38:18 +0300

    FEATURE: print llvm opt version and clang version

--- platform/vars.cmake
+++ platform/vars.cmake
@@ -19,5 +19,27 @@ if(NOT RESULT_FIND_OPT)
     message(FATAL_ERROR "opt binary not found at LLVM_ADIN_PATH path: ${LLVM_ADIN_PATH}")
 endif()
 
+add_custom_command(
+  OUTPUT opt_file_find
+  COMMAND ${RESULT_FIND_OPT} --version
+  USES_TERMINAL
+  DEPENDS RESULT_FIND_OPT)
+
+exec_program(${RESULT_FIND_OPT}
+  ARGS --version
+  OUTPUT_VARIABLE LLVM_OPT_VERSION
+  )
+
+
+cmake_print_variables(LLVM_OPT_VERSION)
+
+exec_program(clang
+  ARGS --version
+  OUTPUT_VARIABLE CLANG_VERSION
+  )
+
+
+cmake_print_variables(CLANG_VERSION)
+
 
 set(REMCU_VM_PATH ${CMAKE_CURRENT_LIST_DIR}/../)
