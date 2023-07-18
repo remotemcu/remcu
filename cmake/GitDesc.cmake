0e07d40f438e06e96f87d6a8dc69d3221cbcfe44
Evgeny Chormonov <en.chormonov@gmail.com>
2023-07-18 15:26:32 -0700

    FIX:  no git problem on Windows OS

--- cmake/GitDesc.cmake
+++ cmake/GitDesc.cmake
@@ -1,31 +1,32 @@
 function(get_git_describe repo_path return_var)
 
 	find_package (Git)
+	if(NOT WIN32)
+		if (GIT_FOUND)
+			message("git found: ${GIT_EXECUTABLE} in version ${GIT_VERSION_STRING}")
+			execute_process(
+				COMMAND ${GIT_EXECUTABLE} -C ${repo_path} describe --tags --abbrev=0
+				OUTPUT_VARIABLE LAST_TAG)
 
-	if (GIT_FOUND)
-		message("git found: ${GIT_EXECUTABLE} in version ${GIT_VERSION_STRING}")
-		execute_process(
-			COMMAND ${GIT_EXECUTABLE} -C ${repo_path} describe --tags --abbrev=0
-			OUTPUT_VARIABLE LAST_TAG)
+			string (REGEX MATCHALL "([^/]+)[\^{}]\n" TEMP ${LAST_TAG})
+			string (REGEX REPLACE "\n" ""  LAST_TAG ${TEMP})
 
-		string (REGEX MATCHALL "([^/]+)[\^{}]\n" TEMP ${LAST_TAG})
-		string (REGEX REPLACE "\n" ""  LAST_TAG ${TEMP})
+			execute_process(
+			    COMMAND ${GIT_EXECUTABLE} -C ${repo_path} rev-parse --short HEAD
+			    OUTPUT_VARIABLE COMMIT_HASH)
 
-		execute_process(
-		    COMMAND ${GIT_EXECUTABLE} -C ${repo_path} rev-parse --short HEAD
-		    OUTPUT_VARIABLE COMMIT_HASH)
+			string (REGEX MATCHALL "([^/]+)[\^{}]\n" TEMP ${COMMIT_HASH})
+			string (REGEX REPLACE "\n" ""  COMMIT_HASH ${TEMP})
 
-		string (REGEX MATCHALL "([^/]+)[\^{}]\n" TEMP ${COMMIT_HASH})
-		string (REGEX REPLACE "\n" ""  COMMIT_HASH ${TEMP})
+			message("git : ${LAST_TAG} @ ${COMMIT_HASH}")
 
-		message("git : ${LAST_TAG} @ ${COMMIT_HASH}")
+			set(ret ${LAST_TAG}-${COMMIT_HASH})
 
-		set(ret ${LAST_TAG}-${COMMIT_HASH})
+		else()
 
-	else()
-
-		set(ret "no_git_info")
-	endif (GIT_FOUND)
+			set(ret "no_git_info")
+		endif (GIT_FOUND)
+	endif(NOT WIN32)
 
 	set(${return_var} ${ret} PARENT_SCOPE)
 endfunction()
